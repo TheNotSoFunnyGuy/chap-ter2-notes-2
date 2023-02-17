128194aa6d627922ca091cb1b9e41a7f7c07e6a4
uquo <uquo@fresnou.org>
2023-02-17 10:27:50 -0800

    initial commit

---
+++ whileLoops1.js
@@ -0,0 +1,85 @@
+const readLine = require('readLine-sync');
+
+//1
+// let a = 0;
+// while (a<26){
+//     console.log(a);
+//     a++;
+// }
+
+//2
+// let b = 0;
+// while (b<51){
+//     console.log(b);
+//     b++;
+// }
+
+//3
+// let c =-10;
+// while (c<11){
+//     console.log(c);
+//     c++;
+// }
+
+//4
+// let d = -50 ;
+// while (d<1){
+//     console.log(d);
+//     d++;
+// }
+
+//5
+// let e = 2;
+// while (e<101){
+//     console.log(e);
+//     e++;
+//     e++;
+// }
+
+//6
+// let f = 99;
+// while (f>0){
+//     console.log(f);
+//     f--;
+//     f--;
+// }
+
+//7
+// let g = 0;
+// while (g<100){
+//     if (0 == g % 3){
+//         console.log(g);
+//     }
+//     g++;
+// }
+
+//8
+// let h = 0;
+// while (h<100){
+//     if (0 == h % 5){
+//         console.log(h);
+//     }
+//     h++;
+// }
+
+//9
+// let i = 0;
+// while (i <= 100){
+//     if (i < 10){
+//         console.log(i);
+//     }else if (i > 25){
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+//10
+let j = 0;
+while (j <= 100){
+    if (j < 10){
+        console.log(j);
+    }else if (j > 20){
+        console.log(j);
+    }
+    j++;
+}
