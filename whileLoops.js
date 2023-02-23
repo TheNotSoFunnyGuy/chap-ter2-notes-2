551ac1d68367414aee3cb3752566a730b884f52c
uquo <uquo@fresnou.org>
2023-02-23 09:37:27 -0800

    initial commit

--- whileLoops.js
+++ whileLoops.js
@@ -24,12 +24,19 @@ const readLine = require('readLine-sync');
 //     w++;
 // }
 
-let start = 0;
-while (start <= 100){
-    if (start < 10){
-        console.log(start);
-    }else if (start > 25){
-        console.log(start);
-    }
-    start++;
+// let start = 0;
+// while (start <= 100){
+//     if (start < 10){
+//         console.log(start);
+//     }else if (start > 25){
+//         console.log(start);
+//     }
+//     start++;
+//}
+
+let answer = readLine.question("Enter y or n: ")
+while (answer != "y" && answer !="n" && answer != "YES"){
+    console.log("Invalid choice");
+    answer = readLine.question("Enter y or n: ");
 }
+console.log(`You Typed ${answer}`);
---
+++ whileLoops2.js
@@ -0,0 +1,65 @@
+const readLine = require("readline-sync");
+
+//11
+// let quit = readLine.question("Quit? y or n: ")
+// while (quit != "y" && quit !="n"){
+//     console.log("Invalid choice");
+//     quit = readLine.question("Quit? y or n: ");
+// }
+
+//12
+// let quit2 = readLine.question("Quit? y or n: ")
+// while (quit2 != "Y" && quit2 !="y" && quit2 !="N" && quit2 !="n"){
+//     console.log("Invalid choice");
+//     quit2 = readLine.question("Quit? y or n: ");
+// }
+
+//13
+// let choose = readLine.question("Choose A, B, or C: ")
+// while (choose != "A" && choose !="B" && choose !="C"){
+//     console.log("Invalid choice");
+//     choose = readLine.question("Choose A, B, or C: ");
+// }
+// console.log(`You Typed ${choose}`);
+
+//14
+// let choose2 = readLine.question("Choose 1, 2, or 3: ")
+// while (choose2 != "1" && choose2 !="2" && choose2 !="3"){
+//     console.log("Invalid choice");
+//     choose2 = readLine.question("Choose 1, 2, or 3: ");
+// }
+// console.log(`You Typed ${choose2}`);
+
+//15
+// let choose3 = readLine.question("Choose a number between 0 and 10: ")
+// while (choose3 != "0" && choose3 !="1" && choose3 !="2" && choose3 != "3" && choose3 != "4" && choose3 != "5" && choose3 != "6" && choose3 != "7" && choose3 != "8" && choose3 != "9" && choose3 != "10"){
+//     console.log("Invalid choice");
+//     choose3 = readLine.question("Choose a number between 0 and 10: ");
+// }
+// console.log(`You Typed ${choose3}`);
+
+//16
+let positive = readLine.question("Enter a positive number: ")
+while (positive != "0"){
+    console.log("Not positive");
+    positive = readLine.question("Enter a positive number: ");
+}
+
+//17
+
+
+//18
+
+
+//19
+// let enter = readLine.question("Enter a number between 0 and 10 or 20 and 30: ")
+// while (enter != "0" && enter !="1" && enter !="2" && enter != "3" && enter != "4" && enter != "5" && enter != "6" && enter != "7" && enter != "8" && enter != "9" && enter != "10"
+//        && enter != "20" && enter !="21" && enter !="22" && enter != "23" && enter != "24" && enter != "25" && enter != "26" && enter != "27" && enter != "28" && enter != "29" && enter != "30"){
+//     console.log("Invalid choice");
+//     enter = readLine.question("Enter a number between 0 and 10 or 20 and 30: ");
+// }
+// console.log(`You Typed ${enter}`);
+
+//20
+
+
