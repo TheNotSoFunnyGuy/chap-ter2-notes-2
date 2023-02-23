06ae4d69cf0b7a7b574048c79c3b5e172e0946b3
uquo <uquo@fresnou.org>
2023-02-23 10:29:01 -0800

    initial commit

---
+++ forLoops2.js
@@ -0,0 +1,82 @@
+//1
+// for(let i = 0; i<= 150; i++){
+//     console.log(i);
+// }
+
+//2
+// for(let i = 0; i<= 40; i++){
+//     console.log(i*i);
+// }
+
+//3
+// for(let i = 0; i<= 30; i++){
+//     console.log(i*i*i);
+// }
+
+//4
+// for(let i = 0; i<= 700; i++){
+//     if(i % 5 == 0) {
+//         console.log(i);
+//     }
+// }
+
+//5
+// for(let i = 350; i<= 1000; i++){
+//     if(i % 10 == 0) {
+//         console.log(i);
+//     }
+// }
+
+//6
+// let sum = 0;
+// for(let i = 1; i <= 50; i++){
+//     sum += i;
+// }
+// console.log(`sum: ${sum}`);
+
+//7
+// let sum = 0;
+// for(let i = 35; i <= 70; i++){
+//     sum += i;
+// }
+// console.log(`sum: ${sum}`);
+
+//8
+// let product = 1;
+// for(let i = 1; i <= 15; i++){
+//     product *= i;
+// }
+// console.log(`Product: ${product}`);
+
+//9
+// let product = 1;
+// for(let i = 10; i <= 35; i++){
+//     product *= i;
+// }
+// console.log(`Product: ${product}`);
+
+//10
+// for(let i = 0; i<= 11; i++){
+//     console.log("Happy Holidays!");
+// }
+
+//11
+
+
+//12
+// let sum = 0;
+// for(let i = 100; i <= 200; i++){
+//     if(i % 2 == 1) {
+//         sum += i;
+//     }
+// }
+// console.log(`sum: ${sum}`);
+
+//13
+let product = 1;
+for(let i = 25; i <= 50; i++){
+    if(i % 2 == 1) {
+        product += i;
+    }
+}
+console.log(`sum: ${product}`);
